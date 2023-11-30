4ebd8f27eeb50a1e06e5a9f71c3893457e9d72fa
Joshua Dada <joshuadada1996@gmail.com>
2023-11-30 07:40:27 +0000

    Update script.js
    made a chang eto location

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,128 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// Destructuring
+//allows you to take values from arrays or objects and set them to individual variables
+
+//Rest Operator
+//allows you to get the rest of the values of the array or objects and applies that into an array or object
+
+//Spread Operator
+//Same syntax (...) as the Rest Operator, and can be used in both arrays and objects
+//Allows you to create a new array with all values and adds a new one to the end
+//Takes all the values out of the array and puts them into a brand new array and the new value without having a seperated array
+//With objects, allows you to add new properties and update existing ones
+//Spreads out properties from object to a new object
+// can also use it to update/override properties from an object
+// will only work if applied after a property is declared so at the end of the updated object
+const book = getBook(3);
+book;
+// const title = book.title;
+// const author = book.author;
+
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
+  book;
+
+console.log(author, title, genres);
+
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+console.log(primaryGenre, secondaryGenre, otherGenres);
+
+const newGenres = [...genres, "epic fantasy"];
+newGenres;
+
+const updatedBook = {
+  ...book,
+  //Adding a new property
+  moviePublicationDate: "2001-12-19",
+  // Overwriting an existing property
+  pages: 1210,
+};
+updatedBook;
+
+//Template Literals
+//Template Literals is an ES6 Javascript feature that allows you to create strings that contains javascript variables or javascript expression inside of a string
+// Template literals use back ticks `` instead of normal speech marks ''
+//Anything that can produce a value can be placed in the block ${}
+
+//Arrow function
+//a new way to write a function, made in ES6
+//Better for one liner functions
+
+//old way of writing function, better for longer functions
+// function getYear(str) {
+//   return str.split("-")[0];
+// }
+//multiple liners will need the return keyword and wirly braces
+//important for call back functions
+const getYear = (str) => str.split("-")[0];
+console.log(getYear(publicationDate));
+const summary = `${title}, a ${pages}--page long book was written by ${author} and published in ${getYear(
+  publicationDate
+)}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+summary;
+
+//Ternary Operator
+//To define values based on a condition
+// Can be used in situations when you cant use an if/else statement
+//Has three operands/three parts in order to work
+// has a condition, then a result of the operation if the condition is true then the results of if the operation is false
+// similar to a function, it will always return a value
+//to simplify, if pages is greater 1000, then value is a thousand if not then value is less than 1000
+// if else statement is a statement so it doesn't return a value so it's not possible to use in certain scenarios
+const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
+
+//Short Circuiting on &&
+//e.g. and and or can short circuit
+//in certain conditions, the operator will return the first value and not look at the second one.
+//and operator short circuits when the first value is false then immediately returns that first value
+// if value is false, immediately returns false
+console.log(true && "string");
+console.log(false && "string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy: 0, '', null, undefined
+// truthy is anything that isn't falsy
+console.log("jonas" && "Some string");
+console.log(0 && "Some String");
+
+//Short Circuiting on ||
+//This is the opposite of the and where it short circuits whenever the first operand is true, then returns it
+//Will only short circuit if the first value is true and doesn't look at the second value
+console.log(true || "Some String");
+console.log(false || "Some String");
+
+console.log(book.translations.spanish);
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
+//This can go wrong, because it works for falsy values
+//because one of the values had 0 review counts, it instead reads no data instead of 0, which is incorrect information, so sometimes short circuiting may not be good
+// console.log(book.reviews.librarything.reviewsCount);
+// const countWrong = book.reviews.librarything.reviewsCount || "no data";
+// countWrong;
+
+//Nullish coalescing operator
+// //New JS operator, which works similar to \\ but also short circuit for falsy valuees
+// const count = book.reviews.librarything.reviewsCount ?? "no data";
+// count;
+
+//Optional Chaining
+// add value from two different objects together - for example two different review count with different key names from the parent object
+// very easy
+//it's good to use optional chaining as much as possible
+//it is a ? before the end of the object, if a value is undefined, it javascript will no longer try to read, the lower value, e.g. reviewsCount
+// the ?? e.g. the Nullish coalescing operator operator helps with situations like this, so if there is no value it can display another value, in this case 0
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews.goodreads?.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  librarything;
+  return goodreads + librarything;
+}
+console.log(getTotalReviewCount(book));
